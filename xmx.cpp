e8e400e71c4d211a5aee7a6ff21f9fedaac05149
ZEMINGMA <1292517305@qq.com>
2022-09-24 17:57:58 +0800

    another test

--- xmx.cpp
+++ xmx.cpp
@@ -6,7 +6,7 @@ int main()
 {
     string str[]={"Mon is happy 8!","Tue is happier 8!","Wed is the happiest 8!","Thu is tired 8!","Fri is unknown 8!"};
     string str2="徐梦茜把我的环境弄乱了！";
-    string str3="进行一个小测试！";
+    string str3="xmx is pig";
     for(int i=0;i<5;i++)
     {
        cout<<str[i] << endl;
