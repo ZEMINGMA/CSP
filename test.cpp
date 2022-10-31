afc338c29d328c7d552b8fdd16318f3b8b2b1b98
ZEMINGMA <1292517305@qq.com>
2022-10-31 19:50:01 +0800

    10-31

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m,n,ans;
+    scanf("%d xmxsz %d",&m,&n);//
+    ans=m-n;
+    printf("%d",ans);
+    system("pause");
+    return 0;
+}
