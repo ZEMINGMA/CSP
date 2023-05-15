14527d12943afe20b0708bc55167d80fcc8303be
ZEMINGMA <1292517305@qq.com>
2023-05-15 11:43:55 +0800

    27-02 optimize

--- 27th/2.cpp
+++ 27th/2.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
+const int maxn=3e5+5;
+int n;
+int x;
+int sum=0;
+int f[maxn];
+int a[31];
 
 int main()
 {
-    int bag;
+    cin>>n>>x;
+    for(int i=1;i<=n;++i){
+        cin>>a[i];
+        sum+=a[i];
+    }
+
+    int m=sum-x;
+
+    for(int i=1;i<=n;++i)
+        for(int j=m;j>=a[i];j--)
+            f[j]=max(f[j],f[j-a[i]]+a[i]);
+    cout<<sum-f[m];
+    system("pause");
     return 0;
 }
