f0ac1a9564355144095155c4d93c143d790abd74
ZEMINGMA <1292517305@qq.com>
2023-07-12 22:21:37 +0800

    bfs

--- GRAPH/BFS/maze.cpp
+++ GRAPH/BFS/maze.cpp
@@ -4,49 +4,53 @@
 using namespace std;
 
 const int N=105;
-struct  Node
-{
-    int x;
-    int y;
-};
-
 int n,m;
 int g[N][N];
-int dis[N][N];
 bool vis[N][N];
+int dis[N][N];
 int dx[4]={0,0,-1,1};
 int dy[4]={1,-1,0,0};
 
-void bfs(int x,int y){
-    queue<Node> q;
-    Node node;
-    node.x=x;
-    node.y=y;
-    q.push(node);
-    vis[x][y]=true;
+struct Node
+{
+    int x,y;
+};
 
+queue<Node> q;
+
+void bfs(int x,int y)
+{
+    Node tt;
+    tt.x=x,tt.y=y;
+    vis[x][y]=true;
+    q.push(tt);
     while(!q.empty()){
-        auto t=q.front();
+        Node t=q.front();
         q.pop();
         for(int i=0;i<4;++i){
             int xx=t.x+dx[i];
             int yy=t.y+dy[i];
-            if(!vis[xx][yy] && g[xx][yy]==0){
-                node.x=xx;
-                node.y=yy;
+            if (g[xx][yy]==0 && !vis[xx][yy])
+            {
+                Node t3;
+                t3.x=xx;
+                t3.y=yy;
                 vis[xx][yy]=true;
-                q.push(node);
                 dis[xx][yy]=dis[t.x][t.y]+1;
+                q.push(t3);
             }
+            
         }
     }
-    cout<<dis[n][m];
 }
 
-int main(){
-    cin>>n>>m;
+int main()
+{
+
+    memset(vis,false,sizeof(vis));   
     memset(g,-1,sizeof(g));
 
+    cin>>n>>m;
     for(int i=1;i<=n;++i){
         for(int j=1;j<=m;++j){
             cin>>g[i][j];
@@ -54,6 +58,7 @@ int main(){
     }
 
     bfs(1,1);
-
+    cout<<dis[n][m];
+    system("pause");
     return 0;
 }
